461e25f2691dcdbb2712076cf58523518348e668
neo <neo@localhost.localdomain>
2020-04-22 18:32:05 +0300

    полное обновление
    file:///home/neo/Documents/uahbot/main.py

--- main.py
+++ main.py
@@ -10,29 +10,29 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message: Message):
-    bot.send_message(message.chat.id, "Howdy, how are you doing?")
+    bot.send_message(message.chat.id, "Привет, how are you doing?")
 
 
 @bot.message_handler(commands=['kurs'])
 def echo_bot(message: Message):
-    main_api = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+      main_api = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
 
-    json_data = requests.get(main_api).json()
+      json_data = requests.get(main_api).json()
 
-    usd = json_data[0]["ccy"] + "  " +  "\n" + json_data[0]["buy"] + "  покупка" + "\n" + \
+      top_text = "Курс валют в отделениях Приват Банка на сегодня:"+"\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n"
+      usd = json_data[0]["ccy"] + "  " +  "\n" + json_data[0]["buy"] + "  покупка" + "\n" + \
           json_data[0]["sale"] + "  продажа" + "\n______________________________\n"
 
-    eur = json_data[1]["ccy"] + "  " + "\n" + json_data[1]["buy"] + "  покупка" + "\n" + \
+      eur = json_data[1]["ccy"] + "  " + "\n" + json_data[1]["buy"] + "  покупка" + "\n" + \
           json_data[1]["sale"] + "  продажа" + "\n______________________________\n"
 
-    rur = json_data[2]["ccy"] + "  " + "\n" + json_data[2]["buy"] + "  покупка" + "\n" + \
+      rur = json_data[2]["ccy"] + "  " + "\n" + json_data[2]["buy"] + "  покупка" + "\n" + \
           json_data[2]["sale"] + "  продажа" + "\n______________________________\n"
 
-    btc = json_data[3]["ccy"] + "  " + "\n" + json_data[3]["buy"] + "  покупка" + "\n" + \
+      btc = json_data[3]["ccy"] + "  " + "\n" + json_data[3]["buy"] + "  покупка" + "\n" + \
           json_data[3]["sale"] + "  продажа" + "\n______________________________\n"
 
-    bot.send_message(message.chat.id, usd + eur + rur + btc)
-
+      bot.send_message(message.chat.id,top_text + usd + eur + rur + btc)
 
 bot.polling(none_stop=True)
 bot.start_polling()
