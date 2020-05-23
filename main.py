83831116d09a2e971556cee8539aef8325f1c542
neo <c4point@gmail.com>
2020-05-23 14:03:32 +0300

    added inline keyboard

--- main.py
+++ main.py
@@ -12,14 +12,19 @@ from config import link as lnk
 
 
 bot = telebot.TeleBot(config.token)
-main_api_local = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
-main_api_remote = "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11"
-mono_api="https://api.monobank.ua/bank/currency"
-
-json_data1 = requests.get(mono_api).json()
+main_api_local = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"#api of PrivatBank 
+main_api_remote = "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11"#api of PrivatBank 
+mono_api="https://api.monobank.ua/bank/currency"#api of Monobank 
+###########################################################################
+json_data1 = requests.get(mono_api).json()         
 json_data2 = requests.get(main_api_remote).json()
-json_data3 = requests.get(main_api_local).json()
-
+json_data3 = requests.get(main_api_local).json()#
+cname1=" - американский доллар"                 #Обьявление переменных
+cname2=" - евро"                                #
+cname3=" - россиский рубль"                     #
+cname4=" - биткоин"
+footer= "\nHere can be  your promo link like "+lnk
+###########################################################################
 class Currency():
     """
     Создаем класс валют чтобы сюда парсить все валюты
@@ -55,7 +60,9 @@ class Currency():
             rateBuy=cur_a.get('buy')
             rateSell=cur_a.get('sale')
             i+=1
-        return name_cur, rateBuy, rateSell  
+        return name_cur, rateBuy, rateSell
+
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -65,13 +72,14 @@ def send_welcome(message):
     u_lname=message.from_user.last_name###################################################################
     u_fname=message.from_user.first_name##################################################################
     
-    keyboard=types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    keyboard=types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
     keyboard1=types.KeyboardButton('Monobank')
     keyboard2=types.KeyboardButton('Privat karta')
-    keyboard3=types.KeyboardButton('Privat otdelenie')
+    keyboard3=types.KeyboardButton('Privat otdelenie')###Кнопки для набора команд вместо клавиатуры
     keyboard4=types.KeyboardButton('/start')
     keyboard.add(keyboard1,keyboard2,keyboard3,keyboard4)
 
+    
     
     if u_name is  None:
         bot.send_message(message.chat.id, "Привет, @"+str(u_name)+" ("+str(u_fname)+" "+str(u_lname)+
@@ -79,16 +87,42 @@ def send_welcome(message):
     else:
         bot.send_message(message.chat.id, "Привет, "+str(u_fname)+" "+str(u_lname)+", как твои дела? Нажимай или набирай - /start",reply_markup=keyboard)   
         
+        
+        
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+  try:
+    if call.message:
+      if call.data == 'good':
+        bot.send_message(call.message.chat.id, 'Спасибо! Ваш голос учтен ')
+      if call.data == 'bad':
+        bot.send_message(call.message.chat.id, 'Буду стараться больше... ')
+ 
+
+  except Exception as e:
+    print(repr(e))
+
+
+        
+        
 @bot.message_handler(content_types='text')
 
 
 def command_bank(message):
 
+    inline_keyboard=types.InlineKeyboardMarkup()
+    inkeyboard1=types.InlineKeyboardButton("Like", callback_data='good')   ####ссылки под  выведеным сообщением
+    inkeyboard2=types.InlineKeyboardButton("Unlike" , callback_data='bad')
+
+    inline_keyboard.add(inkeyboard1, inkeyboard2)
+    
     u_name=message.from_user.username
     u_lname=message.from_user.last_name
     u_fname=message.from_user.first_name
     
     if message.text=='Monobank':
+
+        start_time = datetime.now()
 
         usd_cur=Currency(1, json_data1) #############################
         eur_cur=Currency(2, json_data1) ##Обьявление классов валюты##
@@ -98,26 +132,26 @@ def command_bank(message):
         
         top_text2 ="Курс Monobank для "+str(u_fname)+" "+str(u_lname)+":"+"\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n"
 
-        usd = str(usd_cur.parsing_cur()[0]) + "  " +  "\n" + str(usd_cur.parsing_cur()[1]) + "  покупка" + "\n" + \
+        usd = str(usd_cur.parsing_cur()[0]) + cname1 +  "\n" + str(usd_cur.parsing_cur()[1]) + "  покупка" + "\n" + \
         str(usd_cur.parsing_cur()[2]) + "  продажа" + "\n______________________________\n"
 
-        eur = str(eur_cur.parsing_cur()[0]) + "  " +  "\n" + str(eur_cur.parsing_cur()[1]) + "  покупка" + "\n" + \
+        eur = str(eur_cur.parsing_cur()[0]) + cname2 +  "\n" + str(eur_cur.parsing_cur()[1]) + "  покупка" + "\n" + \
         str(eur_cur.parsing_cur()[2]) + "  продажа" + "\n______________________________\n"
 
-        rur = str(rur_cur.parsing_cur()[0]) + "  " +  "\n" + str(rur_cur.parsing_cur()[0]) + "  покупка" + "\n" + \
+        rur = str(rur_cur.parsing_cur()[0]) +  cname3 +  "\n" + str(rur_cur.parsing_cur()[0]) + "  покупка" + "\n" + \
         str(rur_cur.parsing_cur()[2]) + "  продажа" + "\n______________________________\n"
         
 
-        footer= "\nHere can be  your promo link like "+lnk
-
         if  u_name is None:
-            bot.send_message(message.chat.id,top_text2 + usd + eur + rur + footer)
+            bot.send_message(message.chat.id,top_text2 + usd + eur + rur + footer, reply_markup=inline_keyboard)
+            print(datetime.now() - start_time)
         else:
-            bot.send_message(message.chat.id,top_text + usd + eur + rur + footer)
-
+            bot.send_message(message.chat.id,top_text + usd + eur + rur + footer, reply_markup=inline_keyboard)
+            print(datetime.now() - start_time)
             
     elif message.text=='Privat karta':
-
+        start_time = datetime.now()
+        
         usd_cur=Currency(1, json_data2) #############################
         eur_cur=Currency(2, json_data2) ##Обьявление классов валюты##
         rur_cur=Currency(3, json_data2) #############################
@@ -128,28 +162,30 @@ def command_bank(message):
         
         top_text2 ="Курс Privatbank по картам для "+str(u_fname)+" "+str(u_lname)+" :"+"\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n"
         
-        usd = str(usd_cur.parsing_pb()[0]) + "  " +  "\n" + str(usd_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
+        usd = str(usd_cur.parsing_pb()[0]) + cname1 +  "\n" + str(usd_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
         str(usd_cur.parsing_pb()[2]) + "  продажа" + "\n______________________________\n"
 
-        eur = str(eur_cur.parsing_pb()[0]) + "  " +  "\n" + str(eur_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
+        eur = str(eur_cur.parsing_pb()[0]) + cname2 +  "\n" + str(eur_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
         str(eur_cur.parsing_pb()[2]) + "  продажа" + "\n______________________________\n"
 
 
-        rur = str(rur_cur.parsing_pb()[0]) + "  " +  "\n" + str(rur_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
+        rur = str(rur_cur.parsing_pb()[0]) + cname3 +  "\n" + str(rur_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
         str(rur_cur.parsing_pb()[2]) + "  продажа" + "\n______________________________\n"
 
-        btc = str(btc_cur.parsing_pb()[0]) + "  " +  "\n" + str(btc_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
+        btc = str(btc_cur.parsing_pb()[0]) + cname4 +  "\n" + str(btc_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
         str(btc_cur.parsing_pb()[2]) + "  продажа" + "\n______________________________\n"
 
         
-        footer= "\nHere can be  your promo link like "+lnk
 
         if  u_name is None:
-            bot.send_message(message.chat.id,top_text2 + usd + eur + rur+ btc + footer)
+            bot.send_message(message.chat.id,top_text2 + usd + eur + rur+ btc + footer, reply_markup=inline_keyboard)
+            print(datetime.now() - start_time)
         else:
-            bot.send_message(message.chat.id,top_text + usd + eur + rur+ btc + footer)
+            bot.send_message(message.chat.id,top_text + usd + eur + rur+ btc + footer, reply_markup=inline_keyboard)
+            print(datetime.now() - start_time)
             
     elif message.text=='Privat otdelenie':
+        start_time = datetime.now()
         
         usd_cur=Currency(1, json_data3) #############################
         eur_cur=Currency(2, json_data3) ##Обьявление классов валюты##
@@ -160,30 +196,32 @@ def command_bank(message):
         
         top_text2 ="Курс Privatbank по отделениям для "+str(u_fname)+" "+str(u_lname)+"  :"+"\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n"
         
-        usd = str(usd_cur.parsing_pb()[0]) + "  " +  "\n" + str(usd_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
+        usd = str(usd_cur.parsing_pb()[0]) + cname1 +  "\n" + str(usd_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
         str(usd_cur.parsing_pb()[2]) + "  продажа" + "\n______________________________\n"
 
-        eur = str(eur_cur.parsing_pb()[0]) + "  " +  "\n" + str(eur_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
+        eur = str(eur_cur.parsing_pb()[0]) + cname2 +  "\n" + str(eur_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
         str(eur_cur.parsing_pb()[2]) + "  продажа" + "\n______________________________\n"
 
 
-        rur = str(rur_cur.parsing_pb()[0]) + "  " +  "\n" + str(rur_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
+        rur = str(rur_cur.parsing_pb()[0]) + cname3 +  "\n" + str(rur_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
         str(rur_cur.parsing_pb()[2]) + "  продажа" + "\n______________________________\n"
 
-        btc = str(btc_cur.parsing_pb()[0]) + "  " +  "\n" + str(btc_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
+        btc = str(btc_cur.parsing_pb()[0]) + cname4 +  "\n" + str(btc_cur.parsing_pb()[1]) + "  покупка" + "\n" + \
         str(btc_cur.parsing_pb()[2]) + "  продажа" + "\n______________________________\n"
         
-        footer= "\nHere can be  your promo link like "+lnk
+        
 
         if  u_name is None:
-            bot.send_message(message.chat.id,top_text2 + usd + eur + rur + btc + footer)
+            bot.send_message(message.chat.id,top_text2 + usd + eur + rur + btc + footer, reply_markup=inline_keyboard)
+            print(datetime.now() - start_time)
         else:
-            bot.send_message(message.chat.id,top_text + usd + eur + rur+ btc + footer)
+            bot.send_message(message.chat.id,top_text + usd + eur + rur+ btc + footer, reply_markup=inline_keyboard)
+            print(datetime.now() - start_time)
             
     else :
 
         print("Выбирайте нужную комманду")
         
-        bot.send_message(message.chat.id,  "@"+u_name+' правильнее будет - /start ')
+        bot.send_message(message.chat.id,  "@"+u_name+' правильнее будет - /kurs_beznal (чтобы узнать безналичный курс),\n/kurs_otdelenie (чтобы узнать курс в отделениях), /kurs_mono- для Монобанка, \n /start ')
 
 bot.polling(none_stop=True)
