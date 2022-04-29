d0cd1846de270b9cd51324536c78fb561ba1dadb
Oleg Zubak <c4point@gmail.com>
2022-04-29 17:33:07 +0200

    Added russian warship adress

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ token = "633561599:AAGWRO2n0jm79CScdqIY_haQQoai6yQ30DU" #this is main token
 #token = "988309015:AAF9M77lgGXeYzG-hNGo7C44o7jr1QH2O4c" #this is test token
 
 # link for promo
-link="c4point@gmail.com"
+link="Русский военный корабль, иди нахуй!"
