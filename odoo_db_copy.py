1a6899b19e848a768e20adbc536f623b03bc7350
Oleg Zubak <c4point@gmail.com>
2023-02-23 12:42:45 +0200

    updated

--- odoo_db_copy.py
+++ odoo_db_copy.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-                                                     
 # created by neo                                                            
 # Version-1.0   
-from odoo import fields, modules, api
+
 import config2
-import psycopg2
 import xmlrpc.client
 
 url = "http://34.97.129.95/" # Your local or remote host with "http://"
@@ -22,10 +21,8 @@ for names in conn:
     print(last_entry)
     
 def odoo_bot_send(name_uah,usd, eur, sell_usd, buy_usd, sell_eur, buy_eur):
-    # cur.execute("""SELECT id, sell_data, buy_data, create_uid, write_uid, "name", usd_id, create_date, write_date
-    # FROM public.bot_model ORDER BY write_date DESC  LIMIT 1
-    # ;
-    # """)
+    """Send the data of currency to Odoo data base on remote test server"""
+
     try:
         models.execute_kw(db, uid, password, 'bot.model', 'create', [{'name': name_uah, 'usd': usd, 'eur': eur, 'sell_data_usd': sell_usd, 'buy_data_usd': buy_usd, 'sell_data_eur': sell_eur, 'buy_data_eur': buy_eur}])
         
